81998a43a74387bd7ec08d3753a3e2581a1c0685
jkito <jkito@github>
2022-10-27 21:55:10 +0530

    Init choco package for Qt Installer Framework
    this is a simple chocolatey package that just run the binary
    installer available to download from Qt website

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.qt.io/official_releases/qt-installer-framework/4.4.2/QtInstallerFramework-windows-x64-4.4.2.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'Qt Installer Framework'
+  checksum      = 'a862d08a13fbd531b7ac3a156a5932376f88a655bd0e5f39d0400143e0611463'
+  checksumType  = 'sha256'
+
+  silentArgs    = "in --da -c --al"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+Install-ChocolateyPath "$($env:SystemDrive)\Qt\QtIFW-$($env:ChocolateyPackageVersion)\bin" -PathType 'User'
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Qt Installer Framework'
+  file          = "$($env:SystemDrive)\Qt\QtIFW-$($env:ChocolateyPackageVersion)\Uninstaller.exe"
+  fileType      = 'exe'
+  silentArgs    = "pr -c"
+  validExitCodes= @(0, 3010, 1605, 1614, 1641)
+}
+
+Uninstall-ChocolateyPackage @packageArgs
