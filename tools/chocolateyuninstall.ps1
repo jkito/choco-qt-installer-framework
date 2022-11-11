a5a13c0561c70fb7d59a524a034eade03d60767a
jkito <jkito@github>
2022-11-12 00:30:57 +0530

    remove the location on QtIFW binaries from PATH upon uninstall
    chocolatey doesn't provide a Uninstall-ChocolateyPath helper function
    so it uses the code from https://gitlab.com/DarwinJS/ChocoPackages/-/blob/master/ec2clitools/tools/chocolateyuninstall.ps1 to remove the location from PATH during uninstall

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -9,3 +9,21 @@ $packageArgs = @{
 }
 
 Uninstall-ChocolateyPackage @packageArgs
+
+# Remove QtIFW location from PATH
+# https://gitlab.com/DarwinJS/ChocoPackages/-/blob/master/ec2clitools/tools/chocolateyuninstall.ps1
+# see https://github.com/chocolatey/choco/issues/310
+
+$PathToRemove = "$($env:SystemDrive)\Qt\QtIFW-$($env:ChocolateyPackageVersion)\bin"
+foreach ($path in [Environment]::GetEnvironmentVariable("PATH","User").split(';'))
+{
+  If ($Path)
+  {
+    If (($path -ine "$PathToRemove") -AND ($path -ine "$PathToRemove\"))
+    {
+      [string[]]$Newpath += "$path"
+    }
+  }
+}
+$AssembledNewPath = ($newpath -join(';')).trimend(';')
+[Environment]::SetEnvironmentVariable("PATH",$AssembledNewPath,"User")
